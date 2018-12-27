f7a6d1dda0ea0c66c2a95cca4c9c400757e70886
lifyseit <mail@lifyseit.com>
2018-12-27 13:34:31 +0530

    Updates

---
+++ precache-manifest.3e950d0ea780a3dcf7dd6d235f906fd6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "302f4109e0c0e57471d763759c9dc309",
+    "url": "/lifyse/static/media/v9.302f4109.jpg"
+  },
+  {
+    "revision": "ab8a784f8e0c74d07a6a",
+    "url": "/lifyse/static/css/main.e6eb4896.chunk.css"
+  },
+  {
+    "revision": "28d49f010c908cbde439bdc3eb0eb6ea",
+    "url": "/lifyse/static/media/s1.28d49f01.png"
+  },
+  {
+    "revision": "af0dfac081e0356a30cc",
+    "url": "/lifyse/static/js/1.af0dfac0.chunk.js"
+  },
+  {
+    "revision": "2bdef265660d9ea6bd5a",
+    "url": "/lifyse/static/js/runtime~main.2bdef265.js"
+  },
+  {
+    "revision": "f4ee42e717becf9525f5137a1544ccd3",
+    "url": "/lifyse/static/media/3.f4ee42e7.png"
+  },
+  {
+    "revision": "818d98011d00bc6c362c5cbc5710a3e8",
+    "url": "/lifyse/static/media/ic3.818d9801.png"
+  },
+  {
+    "revision": "c35fbeebff91c45ced6e0000a43a87b3",
+    "url": "/lifyse/static/media/i2.c35fbeeb.jpg"
+  },
+  {
+    "revision": "f70d4c830a213e190e95f21c4c69e1e1",
+    "url": "/lifyse/static/media/I11.f70d4c83.jpg"
+  },
+  {
+    "revision": "758409e2906d9db8d12bcc270342c070",
+    "url": "/lifyse/static/media/i10.758409e2.jpg"
+  },
+  {
+    "revision": "ab8a784f8e0c74d07a6a",
+    "url": "/lifyse/static/js/main.ab8a784f.chunk.js"
+  },
+  {
+    "revision": "b75d7b417c2734149beaf40b6e7e2cca",
+    "url": "/lifyse/static/media/i5.b75d7b41.jpg"
+  },
+  {
+    "revision": "3de13b1b937c88d29f86faffa44de355",
+    "url": "/lifyse/static/media/v3.3de13b1b.jpg"
+  },
+  {
+    "revision": "781d918c4defe880e1850f7fe3593287",
+    "url": "/lifyse/static/media/v4.781d918c.jpg"
+  },
+  {
+    "revision": "655bdd0a9446b101a239dd6f85f09574",
+    "url": "/lifyse/static/media/v5.655bdd0a.jpg"
+  },
+  {
+    "revision": "d4fc4dd27665e1ea214007c4c4865220",
+    "url": "/lifyse/static/media/v6.d4fc4dd2.jpg"
+  },
+  {
+    "revision": "c1ec4776b22d03f77f3e9c6787887db1",
+    "url": "/lifyse/static/media/v1.c1ec4776.jpg"
+  },
+  {
+    "revision": "75d06d49745b17cb6e16d5d22efcbbee",
+    "url": "/lifyse/static/media/v7.75d06d49.png"
+  },
+  {
+    "revision": "af0dfac081e0356a30cc",
+    "url": "/lifyse/static/css/1.206a957e.chunk.css"
+  },
+  {
+    "revision": "978881e0434109c7a0e48db449d23b10",
+    "url": "/lifyse/index.html"
+  }
+];
